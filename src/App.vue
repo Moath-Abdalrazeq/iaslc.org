c65c10487e56d15dfd6f027956ea9b857cce6f0a
Moath-Abdalrazeq <moathabdalhakeem1@gmail.com>
2022-09-03 14:18:47 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import homePage from './components/home-page.vue'
+
+export default defineComponent({
+  components: { homePage },
+  setup() {
+    
+  },
+})
+</script>
+
+
+<template>
+  <home-page></home-page>
+    <router-view/>
+</template>
+
+ 
---
+++ src/components/home-page.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
+<template>
+    <h1> hello</h1>
+</template>
