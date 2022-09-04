ca5902637ed2ffb353ab9d489c0b04832eef0cb2
Moath-Abdalrazeq <moathabdalhakeem1@gmail.com>
2022-09-04 08:18:14 +0300

    add card

--- src/components/home-page.vue
+++ src/components/home-page.vue
@@ -2,6 +2,7 @@
      <div class=" bg-contain h-96 lg:bg-cover  bg- w-full bg-fixed bg-no-repeat  " :style="{'background-image': `url(${headerImg})`  }" >
 
     <menu-header></menu-header>
+    <members-hub-card></members-hub-card>
      </div>
 </template>
 
@@ -9,8 +10,9 @@
 import { defineComponent } from 'vue'
 import menuHeader from './menu-header.vue'
 import headerImg from '../assets/img-header.jpg'
+import MembersHubCard from './members-hub-card.vue'
 export default defineComponent({
-    components:{menuHeader},
+    components:{menuHeader, MembersHubCard},
     setup() {
         return{headerImg}
     },
