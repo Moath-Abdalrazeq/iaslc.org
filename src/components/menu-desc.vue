73d434c2cb5b97888835663a7634d1ebe54a9869
Moath-Abdalrazeq <moathabdalhakeem1@gmail.com>
2022-09-11 10:08:54 +0300

    remove space

--- src/components/menu-desc.vue
+++ src/components/menu-desc.vue
@@ -12,7 +12,7 @@ export default defineComponent({
 </script>
 <template>
   <div class="bg-white h-full w-[30rem]">
-    <div class="flex flex-col p-4 bg-gray-100 w-full">
+    <div class="flex flex-col p-4 bg-gray-100 w-full mt-4">
       <div
         v-for="(item, index) in desc"
         :key="index"
