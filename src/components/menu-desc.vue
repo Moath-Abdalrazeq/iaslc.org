cd0b44aa2fc8364cca1af72bbbe1d0dbf6bd09a5
Moath-Abdalrazeq <moathabdalhakeem1@gmail.com>
2022-09-11 09:59:33 +0300

    remove spaces

--- src/components/menu-desc.vue
+++ src/components/menu-desc.vue
@@ -11,13 +11,15 @@ export default defineComponent({
 });
 </script>
 <template>
-  <div class="flex flex-col p-4 bg-gray-100 h-full w-[30rem]">
-    <div
-      v-for="(item, index) in desc"
-      :key="index"
-      class="mt-4 text-base font-normal"
-    >
-      <a :href="item.code" class="hover:text-red-500">{{ item.text }}</a>
+  <div class="bg-white h-full w-[30rem]">
+    <div class="flex flex-col p-4 bg-gray-100 w-full">
+      <div
+        v-for="(item, index) in desc"
+        :key="index"
+        class="mt-4 text-base font-normal"
+      >
+        <a :href="item.code" class="hover:text-red-500">{{ item.text }}</a>
+      </div>
     </div>
   </div>
 </template>
