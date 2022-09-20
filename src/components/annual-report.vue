f6c3409763269ee9dcaf1237e9973cff7578185a
warddaraghmeh <109684022+warddaraghmeh@users.noreply.github.com>
2022-09-20 20:01:21 +0300

    table

--- src/components/annual-report.vue
+++ src/components/annual-report.vue
@@ -2,114 +2,11 @@
   <div class="">
     <BaseNavigator></BaseNavigator>
   </div>
-  <div class="">
-    <div
-      class="brightness-50 h-screen bg-center bg-cover w-full bg-fixed bg-no-repeat"
-      :style="{ 'background-image': `url(${wordImg})` }"
-    ></div>
-  </div>
+  <WelcomLetter></WelcomLetter>
 
-  <div
-    class="w-full flex px-3 flex-col bg-blue-800 lg:px-4 font-bold text-white"
-  >
-    <div
-      class="z-20 mx-auto -mt-24 lg:-mt-32 flex lg:h-60 h-44 lg:min-w-[28rem] w-60 flex-col items-center justify-center bg-white text-center font-serif text-2xl lg:text-4xl tracking-wider text-black"
-    >
-      <span>ANNUAL REPORT</span>
-      <hr class="mt-1 h-[0.1rem] w-24 bg-red-700" />
-
-      <span>2022</span>
-    </div>
-    <div
-      class="lg:flex lg:flex-row flex flex-col-reverse items-center py-5 mt-5"
-    >
-      <div class="w-max mr-8 lg:mt-0 mt-8">
-        <h1 class="mb-3 text-3xl lg:text-5xl">Welcome Letter</h1>
-        <hr class="mb-5 h-[0.1rem] w-32 bg-red-800" />
-        <p class="text-base max-w-xs lg:max-w-xl w-max text-white leading-7">
-          Dear Colleagues and Friends, Welcome to the 2021 IASLC Annual Report!
-          As we look back on 2021, we see how our lives, our Association and the
-          lung cancer community as a whole continued to be impacted by the
-          pandemic. While we hoped that 2021 would bring us back together to
-          meet in person, that was not to be. However, IASLC was able to produce
-          extraordinary publications and education offerings within a virtual
-          platform.
-        </p>
-        <button class="mt-7 bg-red-700 p-3 text-center uppercase">
-          read the Welcome letter
-        </button>
-      </div>
-      <div class="lg:flex ml-8 w-full lg:justify-between">
-        <div class="lg:mt-10 mt-10 lg:mb-0 mb-5">
-          <div class="ml-5 border-l-2 py-3 px-2 text-xl lg:block hidden">
-            KRISTIN RICHEIMER
-          </div>
-          <img :src="iaslckrImg" alt="iaslckr" class="h-96 lg:h-80 lg:w-64" />
-          <div class="ml-5 border-l-2 py-3 px-2 text-xl">KRISTIN RICHEIMER</div>
-        </div>
-        <div class="">
-          <img :src="doctorImg" alt="iaslckr" class="h-96 lg:h-80 lg:w-64" />
-          <div class="ml-5 mt-4 border-l-2 px-2 text-xl">
-            HEATHER WAKELEE
-            <div>,MD</div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- popup -->
-    <div class="lg:flex items-center leading-10 justify-between font-serif">
-      <youtube-video @on-video-click="videoClicked"></youtube-video>
-      <base-popup v-if="showPopup" @on-close-popup="closePopup">
-        <template #content>
-          <iframe
-            class="h-[28rem] w-full rounded-lg"
-            src="https://www.youtube.com/embed/Ju82sFVclJA"
-          >
-          </iframe>
-        </template>
-      </base-popup>
-      <div class="leading-6 font-sans">
-        <div class="lg:text-2xl text-xl mb-5">IASLC YEAR IN REVIEW</div>
-        <div class="lg:mb-5 lg:max-w-xl max-w-xs">
-          Dr. Heather Wakelee welcomes you to the IASLC 2021 Annual Report.
-        </div>
-        <button class="bg-red-700 p-2 mb-8 lg:mt-1 mt-6 font-sans">
-          READ THE REPORT
-        </button>
-      </div>
-    </div>
-  </div>
   <!--  -->
-  <div
-    class="text-center m-auto max-w-xs lg:max-w-xl lg:font-serif text-2xl text-black my-10"
-  >
-    <div>IASLC'S INTERNATIONAL AND</div>
-    <div>MULTIDISCIPLINARY MEMBERSHIP</div>
 
-    <hr class="m-auto mt-3 w-32 border border-red-800 bg-red-900" />
-  </div>
-  <p class="text-center italic m-auto lg:max-w-6xl font-serif leading-7">
-    Despite the worldwide focus on COVID-19, IASLC members from all corners of
-    the world continued to drive scientific advancements and collaboration.
-    IASLC members worked in incredibly difficult circumstances during the
-    pandemic. Furthering the mission of the IASLC, members were supported in
-    these global efforts by receiving discounted registration fees to meetings
-    and educational events, complimentary access to the Journal of Thoracic
-    Oncology and discounted publication fees for the open-access IASLC Journal,
-    JTO Clinical and Research Reports. (JTO CRR)
-  </p>
-  <button
-    class="m-auto mt-5 mb-5 flex bg-red-600 py-2 px-3 text-center text-white"
-  >
-    READ MORE
-  </button>
-  <h1
-    class="mt-4 text-center font-serif text-2xl max-w-xs lg:max-w-lg m-auto text-black"
-  >
-    MEMBERS BY COUNTRY/REGION
-  </h1>
-  <div class="justify-center flex mt-8 mb-8"><CreateMap></CreateMap></div>
+  <div class="mt-8 mb-8"><CreateMap></CreateMap></div>
   <div class="flex justify-around">
     <char-diagrame></char-diagrame>
     <char-diagrame></char-diagrame>
@@ -119,14 +16,13 @@
 
   <CardsDropdown></CardsDropdown>
   <MeetingsCard></MeetingsCard>
+  <FinancialsTable></FinancialsTable>
+  <CompanyLogo></CompanyLogo>
 </template>
 <script lang="ts">
 import { defineComponent, ref } from "vue";
-import wordImg from "../assets/word.jpg";
-import doctorImg from "../assets/doctor.jpg";
-import iaslckrImg from "../assets/iaslckr.jpg";
+
 import BasePopup from "./base-popup.vue";
-import YoutubeVideo from "./youtube-video.vue";
 import BaseNavigator from "./base-navigator.vue";
 
 import CreateMap from "./create-map.vue";
@@ -134,34 +30,25 @@ import BaseSliderCommites from "./base-slider-commites.vue";
 import CharDiagrame from "./char-diagrame.vue";
 import CardsDropdown from "./cards-dropdown.vue";
 import MeetingsCard from "./meetings-card.vue";
+import FinancialsTable from "./financials-table.vue";
+import WelcomLetter from "./welcom-letter.vue";
+import CompanyLogo from "./company-logo.vue";
 
 export default defineComponent({
   components: {
     BasePopup,
-    YoutubeVideo,
     BaseNavigator,
     CreateMap,
     BaseSliderCommites,
     CharDiagrame,
     CardsDropdown,
     MeetingsCard,
+    FinancialsTable,
+    WelcomLetter,
+    CompanyLogo,
   },
   setup() {
-    let showPopup = ref(false);
-    function videoClicked() {
-      showPopup.value = true;
-    }
-    function closePopup(close: boolean) {
-      showPopup.value = close;
-    }
-    return {
-      wordImg,
-      doctorImg,
-      iaslckrImg,
-      videoClicked,
-      showPopup,
-      closePopup,
-    };
+    return {};
   },
 });
 </script>
---
+++ src/components/financials-table.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="my-5">
+    <span class="flex justify-center text-4xl font-bold">FINANCIALS</span>
+    <hr class="m-auto h-[0.14rem] w-32 bg-red-700 mt-4" />
+  </div>
+  <div class="container px-10 mx-auto">
+    <div class="overflow-x-auto">
+      <table class="min-w-full">
+        <caption
+          class="text-gray-600 text-end border-y-2 opacity-90 text-lg bg-gray-200 p-3"
+        >
+          <span class="lg:mr-20">December 31 (Unaudited)</span>
+        </caption>
+        <thead class="border-b h-10 font-serif text-center">
+          <th></th>
+          <th>
+            <strong>2021</strong>
+          </th>
+          <th>
+            <strong>2020</strong>
+          </th>
+        </thead>
+
+        <tbody>
+          <tr class="border-b-2" v-for="(input, index) in data" :key="index">
+            <td class="py-4">{{ input.info }}</td>
+            <td>{{ input.amount1 }}</td>
+            <td>{{ input.amount2 }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+  <div class="">
+    <button class="bg-red-600 text-white p-2 mt-10">READE MORE</button>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  setup() {
+    let data = ref([
+      {
+        info: "Program Revenues",
+        amount1: "$12,78,644",
+        amount2: "$7,148,662",
+      },
+
+      {
+        info: "Membership Revenues",
+        amount1: "$744,497",
+        amount2: "$632,870",
+      },
+
+      {
+        info: "Other Revenues",
+        amount1: "$927,858",
+        amount2: "--",
+      },
+
+      {
+        info: "Total Revenues",
+        amount1: "$14,465,999",
+        amount2: "$7,781,532",
+      },
+      {
+        info: "",
+        amount1: "",
+        amount2: "",
+      },
+
+      {
+        info: "Direct Program Expenses",
+        amount1: "$7,582,308",
+        amount2: "$4,584,120",
+      },
+
+      {
+        info: "G&A Expenses",
+        amount1: "$5,115,252",
+        amount2: "$5,520,532",
+      },
+
+      {
+        info: "Total Expenses",
+        amount1: "$12,697,560",
+        amount2: "$10,104,652",
+      },
+      {
+        info: "",
+        amount1: "",
+        amount2: "",
+      },
+
+      {
+        info: "Operating Surplus(Deficit)",
+        amount1: "$1,759,439",
+        amount2: "($2,323,120)",
+      },
+
+      {
+        info: "",
+        amount1: "",
+        amount2: "",
+      },
+
+      {
+        info: "Other Income/Expense",
+        amount1: "$950,000",
+        amount2: "$65,296",
+      },
+    ]);
+    return { data };
+  },
+});
+</script>
