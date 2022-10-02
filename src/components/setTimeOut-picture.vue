529ef2be301be3e87cf93e756aa8046a72dc747a
Moath-Abdalrazeq <moathabdalhakeem1@gmail.com>
2022-10-02 15:29:18 +0300

    responsive

--- src/components/setTimeOut-picture.vue
+++ src/components/setTimeOut-picture.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <div class="flex justify-center">
-      <div v-for="(data, index) in tempData" :key="index">
-        <img :src="data" class="h-10 px-8" />
+      <div v-for="(data, index) in tempData" :key="index" class="lg:flex hidden">
+        <img :src="data" class="h-10 px-8  "   />
+      </div>
+      <div v-for="(dataSmall, index) in tempDataSmall" :key="index" class="lg:hidden">
+        <img :src="dataSmall" class="h-10 px-8    "   />
       </div>
     </div>
     <div class="flex justify-end mr-6 items-end mt-8">
@@ -48,54 +51,81 @@ export default defineComponent({
       imgTwelve,
     ]);
     let currentSlide = ref(6);
+    let currentSlideSmall=ref(2)
     let tempIndex = ref(0);
+    let tempIndexSmall=ref(0)
     let tempData = ref([]);
+    let tempDataSmall=ref([])
     function initSlids() {
       tempData.value = [];
       for (let i = tempIndex.value; i < currentSlide.value; i++) {
         tempData.value.push(images.value[i]);
       }
     }
+
+    function initSlidsSmall() {
+      tempDataSmall.value = [];
+      for (let i = tempIndexSmall.value; i < currentSlideSmall.value; i++) {
+        tempDataSmall.value.push(images.value[i]);
+      }
+    } 
+
     function prev() {
-      if (tempIndex.value > 0) {
+      if (tempIndex.value > 0 || tempIndexSmall.value >0 ) {
         currentSlide.value -= 1;
         tempIndex.value -= 1;
+        currentSlideSmall.value -= 1;
+        tempIndexSmall.value -= 1;
         initSlids();
+        initSlidsSmall() 
       }
     }
     function next() {
-      if (images.value.length - 1 >= currentSlide.value) {
+      if (images.value.length - 1 >= currentSlide.value || images.value.length - 1 >= currentSlideSmall.value) {
         currentSlide.value += 1;
         tempIndex.value += 1;
+        currentSlideSmall.value += 1;
+        tempIndexSmall.value += 1;
         initSlids();
+        initSlidsSmall()
       }
     }
     let timeIndex = 0;
+    let timeIndexSmall=0
     let tempSlide = images.value[timeIndex];
+    let tempSlideSmall=images.value[timeIndexSmall];
     function timeOut() {
-      if (images.value.length - 1 >= currentSlide.value) {
+      if (images.value.length - 1 >= currentSlide.value || images.value.length - 1 >= currentSlideSmall.value) {
         next();
       } else {
-        if (images.value.length > timeIndex) {
+        if (images.value.length > timeIndex || images.value.length > timeIndexSmall ) {
           tempSlide = images.value[timeIndex];
           timeIndex++;
+          tempSlideSmall = images.value[timeIndexSmall];
+          timeIndexSmall++;
         } else {
           timeIndex = 0;
           tempSlide = images.value[timeIndex];
           timeIndex++;
+          timeIndexSmall = 0;
+          tempSlideSmall = images.value[timeIndexSmall];
+          timeIndexSmall++;
         }
 
         tempData.value.splice(0, 1);
         tempData.value.push(tempSlide);
+        tempDataSmall.value.splice(0, 1);
+        tempDataSmall.value.push(tempSlideSmall);
       }
     }
     function init() {
       initSlids();
+      initSlidsSmall()
       setInterval(timeOut, 1500);
     }
     onMounted(init);
 
-    return { tempData, prev, next };
+    return { tempData, prev, next,tempDataSmall };
   },
 });
 </script>
